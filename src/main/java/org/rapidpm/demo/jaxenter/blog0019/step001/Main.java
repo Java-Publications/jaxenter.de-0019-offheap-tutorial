a67d8326a51b94d4afb108665ccc80691b311ed3
Sven Ruppert <sven.ruppert@gmail.com>
2014-11-21 12:44:00 +0100

    first version .. of nothing..

---
+++ src/main/java/org/rapidpm/demo/jaxenter/blog0019/step001/Main.java
@@ -0,0 +1,10 @@
+package org.rapidpm.demo.jaxenter.blog0019.step001;
+
+/**
+ * Created by Sven Ruppert on 21.11.2014.
+ */
+public class Main {
+  public static void main(String[] args) {
+    //und los geht es ....
+  }
+}
